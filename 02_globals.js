d7360ac15ada93603de67a5e931b178a9f3536c5
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2023-02-27 00:02:12 +0530

    Introduction to GLOBALS

---
+++ 02_globals.js
@@ -0,0 +1,8 @@
+console.log(__dirname)  // Gives out the directory name
+console.log(__filename) // Gives out the filename
+
+// setInterval() and setTimeout are available
+setInterval(() => {
+    console.log('hello world')
+}, 1000)
+
