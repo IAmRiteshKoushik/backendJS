084fd32dd7842cdb7a1e4ebe7c0480c5fec47b06
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2023-03-13 11:14:09 +0530

    Complete Alternate Syntax

---
+++ 06_altSyntax_import.js
@@ -0,0 +1,13 @@
+module.exports.items = [
+    'item1',
+    'item2'
+]
+
+const person = {
+    name: 'bob',
+}
+
+// You can also write - module.exports.person
+// But this type of syntax can be seen in some
+// legacy code
+module.exports.singlePerson = person
