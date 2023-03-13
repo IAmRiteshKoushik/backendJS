42698444ac9c2f762593889494630fcedaa41014
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2023-03-13 11:17:08 +0530

    Intro compelte

---
+++ 08-introComplete.js
@@ -0,0 +1,10 @@
+// require() method allows us to get an object 
+// containing all the data regarding the exports
+// of a file.js
+
+const data = require('./06_altSyntax_import')
+console.log(data)
+
+// This is similar to running another script
+// from current script.
+require('./07-mind-greanade')
