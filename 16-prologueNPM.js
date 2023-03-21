56e01f05b52c7f421c2654d0dc50253aebd6b61b
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2023-03-21 22:24:42 +0530

    prologue to NPM

---
+++ 16-prologueNPM.js
@@ -0,0 +1,10 @@
+// local dependency - use it in any project
+// npm i <packageName>
+
+// gloabl dependency - use it in any project
+// npm install -g <packageName>
+// sudo npm install -g <packageName> (mac)
+
+// We use local dependencies in order to use
+// "virtul-like" environments
+
