e148bd5596b4c3eee11fb0ed978fbde9f57a0843
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2023-03-21 22:26:33 +0530

    prologue to NPM

--- 16-prologueNPM.js
+++ 16-prologueNPM.js
@@ -8,3 +8,10 @@
 // We use local dependencies in order to use
 // "virtul-like" environments
 
+// package.json - manifest file (stores important
+// info about project/package)
+// manual approach (create package.json is the root
+// , create properties etc)
+// npm init (step by step, press enter to skip)
+// npm init -y (everything default)
+
