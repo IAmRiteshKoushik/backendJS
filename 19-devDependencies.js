def9e11265e21f99a42ad0f818982dbf155b1b4b
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2023-03-23 11:11:24 +0530

    Understand dev dependencies and nodemon

---
+++ 19-devDependencies.js
@@ -0,0 +1,8 @@
+// JSON does not support comments, hence all the learnings are being written in a JavaScript file. 
+
+// Dev dependencies are the packages used only during the developlent phase of the project and are listed under
+// the dev dependencies key of the JSON file.  
+
+// Installation procedure ->
+// npm install <dependency> -D (or)
+// npm install <dependency> --save-dev
---
+++ 20-aboutNodemon.js
@@ -0,0 +1,11 @@
+// Nodemon is a command-line tool that helps 
+// with the speedy development of Node.js 
+// applications. It monitors your project
+// directory and automatically restards your node
+// applcation when it detects any changes.
+
+// THis means that you do not have to stop and
+// restart your application in order for your changes
+// to taek effect. 
+
+// Basically, the live server equivalent in backend. 
