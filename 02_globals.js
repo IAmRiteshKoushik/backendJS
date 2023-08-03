71790aa300746d27545acec86b200ad12bdd8f24
Ritesh Koushik <riteshkoushik39@gmail.com>
2023-08-03 20:44:28 +0530

    Added comments to 02_globals.js

--- 02_globals.js
+++ 02_globals.js
@@ -1,6 +1,9 @@
 console.log(__dirname)  // Gives out the directory name
 console.log(__filename) // Gives out the filename
 
+// require - function to use modules (CommonJS)
+// module - info about current module (file)
+// process - info about env where the program is being executed
 // setInterval() and setTimeout are available
 setInterval(() => {
     console.log('hello world')
