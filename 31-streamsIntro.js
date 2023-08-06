31b772d113e9cec3fe541184183e002049ed437f
Ritesh Koushik <riteshkoushik39@gmail.com>
2023-08-06 18:44:48 +0530

    Introduction to streams

---
+++ 31-streamsIntro.js
@@ -0,0 +1,9 @@
+// Streams are of 4 types
+// 1 - Writable
+// 2 - Readable
+// 3 - Duplex (Readable and Writable)
+// 4 - Transform
+//
+// Streams extend the eventEmitter class so all 
+// the methods of that class can be applied here
+// as well. 
